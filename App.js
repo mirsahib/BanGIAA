0f34f49df7ebbc21561031b09fe5f39ae67ed1c8
Mir Sahib <mirsahib24@gmail.com>
2021-11-15 20:17:19 +0600

    :construction: working on camera page

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Camera from './component/CameraScreen';
+import SignIn from './component/SignIn';
 import SignUp from './component/SignUp';
 
 export default function App() {
   return (
-    <SignUp/>
+    <Camera/>
   );
 }
