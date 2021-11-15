17b1301f70b5be26fc50414563c8dd000624983c
Mir Sahib <mirsahib24@gmail.com>
2021-11-15 19:22:43 +0600

    :hammer: Done with initial design for sign up page

--- component/SignUp.js
+++ component/SignUp.js
@@ -1,114 +1,112 @@
-import React,{useState} from 'react';
-import { StyleSheet, Text, View,Image,TextInput, KeyboardAvoidingView, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import colors from '../assets/colors/colors';
 import welcomeImg from '../assets/images/welcome.png'
 
-export default function SignUp(){
+export default function SignUp() {
     const [isChecked, setChecked] = useState(false);
 
     return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <Image style={styles.welcomeImg} source={welcomeImg}></Image>    
-            </View>
-            <View style={styles.formContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder={'Enter Email Address'}
-                    value={''}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder={'Enter Password'}
-                    value={''}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder={'Confirm Password'}
-                    value={''}
-                />
-                <View style={{flexDirection:'row',justifyContent:'center'}}>
-                    <Checkbox
-                        style={{marginHorizontal:15}}
-                        value={isChecked}
-                        onValueChange={setChecked}
-                        color={isChecked ? colors.tertiary : undefined}
-                    />
-                    <Text style={styles.paragraph}>Anynomous Sign In</Text>
+        <KeyboardAvoidingView style={{ flex: 1 }}>
+                <View style={styles.container}>
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.welcomeImg} source={welcomeImg}></Image>
+                    </View>
+                    <View style={styles.formContainer}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder={'Enter Email Address'}
+                            value={''}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder={'Enter Password'}
+                            value={''}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder={'Confirm Password'}
+                            value={''}
+                        />
+                        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                            <Checkbox
+                                style={{ marginHorizontal: 15 }}
+                                value={isChecked}
+                                onValueChange={setChecked}
+                                color={isChecked ? colors.tertiary : undefined}
+                            />
+                            <Text style={{color:'white'}}>Anynomous Sign In</Text>
+                        </View>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <View style={{ paddingHorizontal: 10 }} >
+                            <TouchableOpacity style={styles.buttonStyle}>
+                                <Text style={{ color: 'white' }}>Sign Up</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{ paddingHorizontal: 10 }}>
+                            <TouchableOpacity style={styles.buttonStyle}>
+                                <Text style={{ color: 'white' }}>Clear</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.buttonContainer}>
-                <View style={{paddingHorizontal:10}} >
-                    <TouchableOpacity style={styles.buttonStyle}>
-                        <Text style={{color:'white'}}>Sign Up</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={{paddingHorizontal:10}}>
-                    <TouchableOpacity style={styles.buttonStyle}>
-                        <Text style={{color:'white'}}>Clear</Text>
-                    </TouchableOpacity>
-                </View>        
-            </View>
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor:colors.primary,
-      fontFamily:'Poppins',
-      fontWeight:'bold',
-      paddingTop:40
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.primary,
+        fontFamily: 'Poppins',
+        fontWeight: 'bold',
+        paddingTop: 40
     },
-    imageContainer:{
-        flex:2,
-        justifyContent:"center"
+    imageContainer: {
+        flex: 2,
+        justifyContent: "center"
     },
-    formContainer:{
-        flex:2,
+    formContainer: {
+        flex: 2,
     },
-    annynomousContainer:{
-        flexDirection:'row',
-        justifyContent:'center'
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingTop: 15
     },
-    buttonContainer:{
-        flex:1,
-        flexDirection:'row',
-        justifyContent:'space-between',
-        paddingTop:15
-    },
-    welcomeImg:{
-        width:300,
-        height:260
+    welcomeImg: {
+        width: 300,
+        height: 260
     },
     input: {
         height: 50,
-        width:340,
+        width: 340,
         margin: 12,
-        borderRadius:20,
-        borderWidth:1,
-        borderColor:colors.tertiary,
-        backgroundColor:'white',
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: colors.tertiary,
+        backgroundColor: 'white',
         padding: 10,
-        fontWeight:"bold",
-        fontSize:18,
-        color:"#2F2E41",
-        opacity:60
-      },
-    inputFocused:{
-        backgroundColor:colors.tertiary
+        fontSize: 18,
+        color: "#2F2E41",
+        opacity: 60,
+        paddingLeft:15
     },
-    buttonStyle:{
-        width:150,
-        minHeight:50,
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius:20,
-        backgroundColor:colors.tertiary
+    inputFocused: {
+        backgroundColor: colors.tertiary
+    },
+    buttonStyle: {
+        width: 150,
+        minHeight: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        backgroundColor: colors.tertiary
     }
-    
-  });
+
+});
