48c66adb5838468d4760edcbebd8600bfdf189ed
Mir Sahib <mirsahib24@gmail.com>
2021-11-16 11:54:18 +0600

    :hammer: Done with initial camera desing

--- component/CameraScreen.js
+++ component/CameraScreen.js
@@ -1,36 +1,90 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, Platform, View,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, Platform, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
+import colors from '../assets/colors/colors';
 
 
 export default function CameraScreen() {
     const [hasCameraPermission, setCameraPermission] = useState(null)
-    const [type, setType] = useState(Camera.Constants.Type.back);
 
 
     useEffect(async () => {
-        const { status } = await Camera.requestPermissionsAsync()
+        const { status } = await Camera.requestCameraPermissionsAsync()
         setCameraPermission(status === 'granted')
     }, [])
 
     return (
         <View style={styles.container}>
-            <Camera style={styles.camera} type={type}>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => {
-                            setType(
-                                type === Camera.Constants.Type.back
-                                    ? Camera.Constants.Type.front
-                                    : Camera.Constants.Type.back
-                            );
-                        }}>
-                        <Text style={styles.text}> Flip </Text>
-                    </TouchableOpacity>
-                </View>
+            <Camera
+                style={{ flex: 1 }}
+            >
+                <View
+                    style={{
+                        flex: 1,
+                        width: '100%',
+                        backgroundColor: 'transparent',
+                        flexDirection: 'row'
+                    }}>
+                    <View
+                        style={{
+                            position: 'absolute',
+                            left: '5%',
+                            top: '10%',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                backgroundColor: '#fff',
+                                borderRadius: 50,
+                                height: 25,
+                                width: 25
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    textAlign:'center',
+                                    fontSize: 16
+                                }}
+                            >
+                                ⚡️
+                            </Text>
+                        </TouchableOpacity>
+                    </View>{/** end flash mode */}
+                    <View
+                        style={{
+                            position: 'absolute',
+                            bottom: 0,
+                            flexDirection: 'row',
+                            flex: 1,
+                            width: '100%',
+                            padding: 20,
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <View
+                            style={{
+                                alignSelf: 'center',
+                                flex: 1,
+                                alignItems: 'center'
+                            }}
+                        >
+                            <TouchableOpacity
+                                style={{
+                                    width: 70,
+                                    height: 70,
+                                    bottom: 0,
+                                    borderRadius: 50,
+                                    borderWidth:5,
+                                    borderColor:colors.secondary,
+                                    backgroundColor: colors.tertiary
+                                }}
+                            />
+                        </View>
+                    </View>{/** end camera capture  */}
+                </View>{/**end wrapper */}
             </Camera>
-
         </View>
     )
 }
