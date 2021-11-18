6baa4180feebec3f5b10046b4c00cfda33c94161
Mir Sahib <mirsahib24@gmail.com>
2021-11-18 13:31:21 +0600

    :hammer: Add Stack Navigation

---
+++ component/index.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import CameraScreen from './CameraScreen';
+import PreviewScreen from './PreviewScreen';
+import colors from '../assets/colors/colors';
+
+const Stack = createNativeStackNavigator();
+export default function AppContainer(){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='SignUp' component={SignUp} options={{headerShown:false}}/>
+                <Stack.Screen name='SignIn' component={SignIn} options={{headerShown:false}}/>
+                <Stack.Screen name='CameraScreen' component={CameraScreen} options={{headerShown:false}}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
