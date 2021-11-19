1f32c909554ed3ad19322e5cd1b336133fabab66
Mir Sahib <mirsahib24@gmail.com>
2021-11-19 18:17:41 +0600

    :hammer: Add ImageList screen routes

--- component/index.js
+++ component/index.js
@@ -6,6 +6,7 @@ import SignUp from './SignUp';
 import CameraScreen from './CameraScreen';
 import PreviewScreen from './PreviewScreen';
 import colors from '../assets/colors/colors';
+import ImageList from './ImageList';
 
 const Stack = createNativeStackNavigator();
 export default function AppContainer(){
@@ -15,6 +16,7 @@ export default function AppContainer(){
                 <Stack.Screen name='SignUp' component={SignUp} options={{headerShown:false}}/>
                 <Stack.Screen name='SignIn' component={SignIn} options={{headerShown:false}}/>
                 <Stack.Screen name='CameraScreen' component={CameraScreen} options={{headerShown:false}}/>
+                <Stack.Screen name='ImageScreen' component={ImageList} options={{headerShown:false}}/>                
             </Stack.Navigator>
         </NavigationContainer>
     )
