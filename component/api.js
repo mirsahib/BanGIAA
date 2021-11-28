89e8dc3f68a95550531776bbe35e61226b2a436b
Mir Sahib <mirsahib24@gmail.com>
2021-11-28 20:09:20 +0600

    :hammer: Add uploadImage api function

--- component/api.js
+++ component/api.js
@@ -45,8 +45,28 @@ const uploadData = async (url, data) => {
     }
 }
 
+const uploadImage = async (url, uri) => {
+    try {
+        let base64Img = `data:image/jpg;base64,${uri}`
+        let imageData = { file: base64Img, upload_preset: "annotationImage" }
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(imageData),
+        });
+        return await response.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
 export {
     createUser,
     readUser,
-    uploadData
+    uploadData,
+    uploadImage
 }
