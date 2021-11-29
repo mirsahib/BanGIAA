06d3f0b9dc4b4970334a386af28779ab733de771
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-11-29 13:57:29 +0600

    :hammer: #5 createUser webhook check if user email already exists

--- webhooks/createUser.js
+++ webhooks/createUser.js
@@ -16,17 +16,26 @@ exports = async function(payload,response) {
   
     try{
       //console.log('from try',EJSON.parse(payload.body.text()))
-      const result= await users.insertOne(body);
-      console.log("from db",result.insertedId)
-      if(result){
-        const id = result.insertedId
-        console.log('userId',id)
-        response.setBody(JSON.stringify({userId:id,message:"saved"}))
-      }else{
-        response.setBody(JSON.stringify({error:"user not saved"}))
+        const user= await users.findOne({"email" : body.email })
+        console.log('user',JSON.stringify(user))
+        if (user){
+            response.setBody(JSON.stringify({error:"Email already exist"}))
+            return {"error":"Email already exist"}
+        }else{
+            const result= await users.insertOne(body);
+            console.log("from db",result)
+            if(result){
+                const id = result.insertedId
+                console.log('userId',id)
+                response.setBody(JSON.stringify({userId:result.insertedId,message:"User created successfully"}))
+                return {"user":"saved"}
+            }else{
+                response.setBody(JSON.stringify({error:"Something went wrong"}))
+                return {"error":"Something went wrong"}
+            }   
       }
-      return {"data":result.insertedId}
     }catch(err){
+      console.log('error',err)
       return err;
     }
   };
