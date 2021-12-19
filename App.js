a3830fe8de460d3effb5ecf481d12516ea9abd19
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-12-19 17:44:06 +0600

    :camera_flash: Add screenshot

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import AppContainer from './component';
-import CameraScreen from './component/CameraScreen';
-import Example from './component/example';
-import PreviewScreen from './component/PreviewScreen';
-import SignIn from './component/SignIn';
-import SignUp from './component/SignUp';
-import SuggestionList from './component/SuggestionList';
 
 export default function App() {
   return (
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+<img src="relative/path/in/repository/to/image.svg" width="128"/>
 ## BanGIAA
 Bangladesh grocery image annotation app
 
